3443a09bcf12b0285a5f2facfd5bbb6383190e55
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2022-04-01 10:45:19 +0700

    change bobor_nilai column to bobot_nilai in database

--- db_webuts.sql
+++ db_webuts.sql
@@ -47,7 +47,7 @@ CREATE TABLE `mahasiswa` (
   `UKM` varchar(255) DEFAULT NULL,
   `UKK` varchar(255) DEFAULT NULL,
   `Organisasi` varchar(255) DEFAULT NULL,
-  `Bobor_Nilai` varchar(255) DEFAULT NULL,
+  `Bobot_Nilai` varchar(255) DEFAULT NULL,
   `Bobot_Angka` int(11) DEFAULT NULL,
   `Email` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -56,7 +56,7 @@ CREATE TABLE `mahasiswa` (
 -- Dumping data untuk tabel `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`ID`, `NIM`, `Angkatan`, `Kelas`, `Jenis_Kelamin`, `Nama`, `Alamat`, `Hobi`, `Tempat_Lahir`, `Tanggal_Lahir`, `Nomor_Handphone`, `Cita-cita`, `Pembimbing_Akademik`, `Nama_MK`, `Kode_MK`, `Dosen_MK`, `UKM`, `UKK`, `Organisasi`, `Bobor_Nilai`, `Bobot_Angka`, `Email`) VALUES
+INSERT INTO `mahasiswa` (`ID`, `NIM`, `Angkatan`, `Kelas`, `Jenis_Kelamin`, `Nama`, `Alamat`, `Hobi`, `Tempat_Lahir`, `Tanggal_Lahir`, `Nomor_Handphone`, `Cita-cita`, `Pembimbing_Akademik`, `Nama_MK`, `Kode_MK`, `Dosen_MK`, `UKM`, `UKK`, `Organisasi`, `Bobot_Nilai`, `Bobot_Angka`, `Email`) VALUES
 (1, 2989, 2018, 'A', 'Laki-Laki', 'Budi', 'Bandung', 'Game', 'Jakarta', '2000-01-20', 2147483647, 'Dokter', 'Dr. Susilo', 'Manajemen', 'M201', 'Dr. Susi', 'Batminton', 'Batminton', 'BEM', 'B', 2, 'budi@gmai.com'),
 (2, 2983, 2018, 'C', 'Laki-Laki', 'Anton', 'Bandung', 'Menyanyi', 'Jambi', '2001-04-29', 2147483647, 'Artis', 'Dr. Susilo', 'Seni rupa', 'M209', 'Dr. Jaka', 'Paduan suara', '-', 'Paduan suara', 'A', 3, 'anton@gmai.com'),
 (3, 2936, 2018, 'B', 'Laki-Laki', 'Raja', 'Bandung', 'Travellinng', 'Sulawesi', '1999-02-09', 2147483647, 'Pengusaha', 'Dr. Bambang', 'Fatografi', 'F230', 'Dr. Nini', 'Fotografer', 'Fotografer', 'BEM', 'A', 3, 'raja@gmail.com'),
